0c92112b013a6f612cb90d2393c1aa0cec625f3d
jinjiangliang <jinjiangliang@gmail.com>
2018-08-20 15:14:30 +0800

    Learning1.13-1.14
    Notes Learning1.13-1.14

---
+++ PythonResearch/NotesVidio113.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Notes for 
+Video 1.1.3: Modules and Methods
+
+"""
+
+import math
+math.pi
+math.sqrt(10)
+math.pi / 2
+
+from math import pi
+
+"""
+Name space
+"""
+import math
+import numpy as np
+math.sqrt
+np.sqrt
+np.sqrt([2,3,4])
+"""
+not possible for math.sqrt()
+math and np have their 
+"""
+name = "Amy"
+type(name)
+dir(name)
+dir(str)
+help(name.upper)
+name.upper()
+"""
+help, not run
+e.g. help(name.upper())
+"""
---
+++ PythonResearch/NotesVidio114.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 15:04:05 2018
+
+@author: 1000877
+
+CC 1.1.3: Modules and Methods
+Suppose that math.sqrt and numpy.sqrt had identical behavior. Are they the same function?
+NO.
+No. Because they belong to different namespaces, Python treats them separately, regardless of their behavior.
+After running import numpy as np , if you want to access the square root function (sqrt() ) from the library numpy , which method would you use?
+np.sqrt()
+//
+Video 1.1.4: Numbers and Basic Calculations
+integer
+power: 
+    123**10
+floor division
+15/2
+15//7   
+//
+underscore operator
+_
+return the latest number;
+//
+factorial operation
+"""
+import math
+math.factorial(3)
