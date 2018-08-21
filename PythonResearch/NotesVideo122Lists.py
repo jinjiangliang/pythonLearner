f57595a35be0330c234bd90a81764e95f9e92107
jinjiangliang <jinjiangliang@gmail.com>
2018-08-21 15:14:23 +0800

    1.22lists

---
+++ PythonResearch/NotesVideo122Lists.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 15:03:16 2018
+
+@author: 1000877
+
+Video 1.2.2: Lists
+
+strings: immutable
+list: mutable
+"""
+numbers = [2,4,6,8]
+numbers[0]
+numbers[-1]
+
+'''
+appedning new number
+'''
+numbers.append(10)
+numbers+x
+type(_)
+
+'''
+reverse
+'''
+numbers.reverse()
+numbers
+names = ['a','c','b']
+names.sort()
+names.reverse()
+names
+sorted.names = sorted(names)
+sorted_names
+len(names)
+
+'''
+x.append(4)
+What do list methods such as reverse and sort return?
+They return nothing because they are in-place methods, meaning they alter the content of the original list.
+'''
